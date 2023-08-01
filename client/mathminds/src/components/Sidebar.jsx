4b17126d85f94bd5357c703f3e35ab07332e7d51
João Pedro Bacarin <112831294+JPBaccarin@users.noreply.github.com>
2023-07-31 21:46:23 -0300

    Update Sidebar.jsx

--- client/mathminds/src/components/Sidebar.jsx
+++ client/mathminds/src/components/Sidebar.jsx
@@ -13,49 +13,44 @@ const Sidebar = () => {
         <div>
             <button
                 onClick={toggleSidebar}
-                className={`fixed top-4 left-4 bg-white dark:bg-gray-500 border-2 border-white/20 rounded-full p-2 z-50 ${isOpen ? 'hidden' : 'block'
+                className={`fixed top-4 left-4 bg-white dark:bg-gray-800 border-2 border-gray-300 dark:border-gray-700 rounded-full p-2 z-40 ${isOpen ? 'hidden' : 'block'
                     }`}
             >
-                <FaBars color='white' size={18} />
+                <FaBars className='dark:fill-white' size={18} />
             </button>
-            <button
-                onClick={toggleSidebar}
-                className={`fixed top-4 left-4 bg-white  dark:bg-gray-500 border-2 border-white/20 rounded-full p-2 z-50 ${isOpen ? 'block' : 'hidden'
+            <div
+                className={`fixed top-0 left-0 h-screen w-52 dark:text-white bg-white dark:bg-gray-800 shadow-lg transform transition-transform z-50 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'
                     }`}
             >
-                <AiOutlineClose color='white' size={18} />
-            </button>
-            {isOpen && (
-                <div
-                    className={`fixed top-0 left-0 w-60 pb-5 bg-white dark:bg-gray-700 bg-opacity-90 backdrop-blur-md z-40 rounded-br-lg shadow-lg ${isOpen ? 'block' : 'hidden'
-                        } text-black dark:text-white`}
+                <button
+                    onClick={toggleSidebar}
+                    className="absolute top-4 right-4 bg-white dark:bg-gray-800 border-2 border-gray-300 dark:border-gray-700 rounded-full p-2"
                 >
-                    <div className="flex flex-col items-center h-full">
-                        <div className="mt-14 mb-5">
-                            <img
-                                src="https://via.placeholder.com/150"
-                                alt="Foto do Usuário"
-                                className="w-16 h-16 rounded-full mt-2"
-                            />
-                            <h1 className="text-xl font-semibold mt-2">Usuário</h1>
-                        </div>
-                        <div className="flex flex-col items-center">
-                            <a href="/learning" className="mb-2 w-full text-center border-y-2 border-red-500 py-2">
-                                <FaRegLightbulb className="inline-block mr-2" />
-                                Learning
-                            </a>
-                            <a href="/" className="mb-2 w-full text-center border-y-2 border-red-500 py-2">
-                                <FaClipboardList className="inline-block mr-2" />
-                                Quiz
-                            </a>
-                            <a href="/essay" className="mb-2   w-full text-center border-y-2 border-red-500 py-2">
-                                <FaPen className="inline-block mr-2" />
-                                Essay
-                            </a>
-                        </div>
+                    <AiOutlineClose className='dark:fill-white' size={18} />
+                </button>
+                <div className="flex flex-col items-center h-full mt-4 py-8">
+                    <img
+                        src="https://via.placeholder.com/150"
+                        alt="Foto do Usuário"
+                        className="w-24 h-24 rounded-full mb-4"
+                    />
+                    <h1 className="text-lg font-semibold">Usuário</h1>
+                    <div className="flex flex-col mt-4">
+                        <a href="/learning" className="mb-2 py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-700 rounded">
+                            <FaRegLightbulb className="inline-block mr-2 fill-red-500" />
+                            Learning
+                        </a>
+                        <a href="/" className="mb-2 py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-700 rounded">
+                            <FaClipboardList className="inline-block mr-2 fill-red-500" />
+                            Quiz
+                        </a>
+                        <a href="/essay" className="mb-2 py-2 px-4  hover:bg-gray-100 dark:hover:bg-gray-700 rounded">
+                            <FaPen className="inline-block mr-2 fill-red-500" />
+                            Essay
+                        </a>
                     </div>
                 </div>
-            )}
+            </div>
         </div>
     );
 };
